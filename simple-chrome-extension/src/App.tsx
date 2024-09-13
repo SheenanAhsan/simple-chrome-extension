87dd41f95d9e471e04312f7fe30ef905749a062e
Sheenan Ahsan <sheenanahsan@gmail.com>
2024-09-13 16:42:15 -0400

    changed App.tsx to make it so extension will make a pop up when button clicked

--- simple-chrome-extension/src/App.tsx
+++ simple-chrome-extension/src/App.tsx
@@ -1,10 +1,18 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const onclick = async () => {
+	let [tab] = await chrome.tabs.query({ active: true});	
+	chrome.scripting.executeScript({
+		target: { tabId: tab.id! },
+		func: () => {
+			alert('Hello from my extension!');
+		}
+	});
+  }
 
   return (
     <>
@@ -16,11 +24,11 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>My Extension</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={onclick}>
+	  Click Me
+	</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
